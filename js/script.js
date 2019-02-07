a0a3d52528a89c815032f85561546c39940c0fc3
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-02-07 13:12:46 +0100

    modified js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ function Phone(brand, price, color) {
 }
 
 Phone.prototype.printInfo = function(){
-console.log('Ten telefon jest marki'+ this.brand + 'kolor jego to' + this.color + 'a cena jego to' + this.price + ".");
+console.log('Ten telefon jest marki '+ this.brand + ' kolor jego to ' + this.color + ' cena jego to ' + this.price + ".");
 }
+
+var iphone6S = new Phone ('Apple','2250','black');
+
+iphone6S.printInfo()
+
+var samsungS6 = new Phone ('Samsung','1500','silver');
+
+samsungS6.printInfo()
+
+var OnePlusOne = new Phone ('One Plus', '2000' , 'gold');
+
+OnePlusOne.printInfo(
+
+)
     
