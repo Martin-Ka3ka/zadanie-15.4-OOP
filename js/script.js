c164b709cdb6602b1429e4f6112a7cb6758e7e31
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-02-09 12:45:44 +0100

    modified file js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
-function Phone(brand, price, color,size) {
+function Phone(brand, color, size) {
    this.brand = brand;
-   this.price = price;
    this.color = color;
    this.size = size;
+   this.price = [];
 }
 
-Phone.prototype.printInfo = function(){
-console.log('Ten telefon jest marki '+ this.brand + ' kolor jego to ' + this.color + ' cena jego to ' + this.price + ' rozmiar ekranu '+ this.size);
+Phone.prototype.printInfo = function () {
+   console.log('Ten telefon jest marki ' + this.brand + ' kolor jego to ' + this.color + ' cena jego to ' + this.price + ' rozmiar ekranu ' + this.size);
+}
+Phone.prototype.addprice = function (brand) {
+   this.price.push(brand);
 }
 
-var iphone6S = new Phone ('Apple','2250','black','4"');
 
-iphone6S.printInfo()
+var iphone6S = new Phone('Apple', 'black', '4"');
 
-var samsungS6 = new Phone ('Samsung','1500','silver','5,6"');
+var samsungS6 = new Phone('Samsung', 'silver', '5,6"');
 
-samsungS6.printInfo()
+var OnePlusOne = new Phone('One Plus', 'gold', '5"');
 
-var OnePlusOne = new Phone ('One Plus', '2000' , 'gold', '5"');
+iphone6S.addprice('2250');
 
-OnePlusOne.printInfo()
+samsungS6.addprice('1500');
 
+OnePlusOne.addprice('1800');
 
+OnePlusOne.printInfo();
 
+samsungS6.printInfo();
 
-    
+iphone6S.printInfo();
